c584923137e158ce8eee7f3853193adb2d8c8874
Armin <46172886+userDeveloped@users.noreply.github.com>
2019-07-04 21:03:15 -0700

    Update ChromeAccess.java
    Make sure

--- ChromeAccess.java
+++ ChromeAccess.java
@@ -7,13 +7,14 @@ import java.util.Properties;
 import javax.mail.Folder;
 import javax.mail.Session;
 import javax.mail.Store;
-public class ChromeAccess {
+public class Catcher {
 
-	private static final String email_id = "alipour_armin@student.smc.edu";
-	private static final String password = "276666Co";
+	private static final String email_id = "lastname_firstname@student.smc.edu";
+	private static final String password = "password";
 	
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","/home/lolwtf/Desktop/Armin/Computer Science/chromedriver");
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver","/home/Desktop/Computer/chromedriver"); // location of chrome driver in your PC
 		
 		  String fromEmail = "";
 		  boolean runner = true;
@@ -23,9 +24,10 @@ public class ChromeAccess {
 		  properties.put("mail.imaps.host", "imap.gmail.com"); //Host Address of Your Mail
 		  properties.put("mail.imaps.port", "993"); //Port number of your Mail Host
 		 
-		  //properties.put("mail.imaps.timeout", "10000");
+		  
 		 
-		  try {
+		  try 
+		  {
 		 
 		   Session session = Session.getDefaultInstance(properties, null); //create a session 
 		   Store store = session.getStore("imaps"); //SET the store for IMAPS
@@ -47,9 +49,9 @@ public class ChromeAccess {
 		       {
 		    	   
 		       
-		         if(findCourse(fromEmail).equals("4382"))
+		         if(findCourse(fromEmail).equals("couse number example : 1234"))
 		        {
-		        	addFast("4382");
+		        	addFast("1234");
 		        	runner = false;
 		        }
 		        else
@@ -71,57 +73,49 @@ public class ChromeAccess {
 		  }
 		 
 		 
+	}
 		 
-
-		 }
-		 
-	
-	
-public static void addFast(String classNumber)
-{
-	WebDriver driver = new ChromeDriver(); 
-    driver.get("https://smccis.smc.edu/smcweb/f?p=420181023:1:5552447647573:1516262:NO::P1_SEMCODE:20190"); 
-    
-    WebElement id = driver.findElement(By.name("P102_LOGIN_ID"));
-    WebElement pass = driver.findElement(By.name("P102_PASSWORD"));
- 
-    id.sendKeys("1516262");
-    pass.sendKeys("276666Co");
-    pass.submit();
-    
-    Select dropdown = new Select(driver.findElement(By.id("P1_SEMCODE")));
-    dropdown.selectByIndex(2);
-    driver.findElement(By.linkText("Add a Class")).click();
-    
-    WebElement textAdd = driver.findElement(By.name("P301_ADDSCTNUM"));
-    textAdd.sendKeys(classNumber);
-   
-    
-    WebElement buttonAdd = driver.findElement(By.id("B18390178385123965523"));
-    buttonAdd.click();
-   
-}
-
-public static String findCourse(String fck)
-{
-  String theString = "";
-  int exiter = 0;
-  if(fck.charAt(0) == 'S' && fck.charAt(1) == 'e' && fck.charAt(2) == 'c')
-  {
-  for(int i = 0 ; i < fck.length(); i++)
-  {
-    if(fck.charAt(i) >= 48 && fck.charAt(i) <= 57)
-    {
-      theString+=fck.charAt(i);
-      if(exiter++ == 3)
-      {
-        break;
-      }
-    }
-  }
-}
-  return theString;
-}
+	public static void addFast(String classNumber)
+	{
+		WebDriver driver = new ChromeDriver(); 
+    		driver.get("https://smccis.smc.edu/smcweb/f?p=420181023:1:5552447647573:1516262:NO::P1_SEMCODE:20190"); 
+   		WebElement id = driver.findElement(By.name("P102_LOGIN_ID"));
+    		WebElement pass = driver.findElement(By.name("P102_PASSWORD"));
+		id.sendKeys("1111111"); //smc student ID
+    		pass.sendKeys("password"); //smc password
+    		pass.submit();
+    		// locating the buttons on website
+		Select dropdown = new Select(driver.findElement(By.id("P1_SEMCODE")));
+    		dropdown.selectByIndex(2);
+    		driver.findElement(By.linkText("Add a Class")).click();
+    		WebElement textAdd = driver.findElement(By.name("P301_ADDSCTNUM"));
+  		textAdd.sendKeys(classNumber);
+   		WebElement buttonAdd = driver.findElement(By.id("B18390178385123965523"));
+    		buttonAdd.click();
+   	}
+	/*
+	*My approach to fix emails that are send inorder to trick this program
+	*/
+	public static String findCourse(String fck)
+	{
+  		String theString = "";
+  		int exiter = 0;
+  		if(fck.charAt(0) == 'S' && fck.charAt(1) == 'e' && fck.charAt(2) == 'c')
+  		{
+  			for(int i = 0 ; i < fck.length(); i++)
+  			{
+    				if(fck.charAt(i) >= 48 && fck.charAt(i) <= 57)
+    				{
+      					theString+=fck.charAt(i);
+      					if(exiter++ == 3)
+      					{
+        					break;
+      					}
+    				}
+  			}
+		}
+  		return theString;
+	}
 
 }
 
